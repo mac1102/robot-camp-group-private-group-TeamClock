84f9342b07b8d391ec7179ac4b76e1309cffc012
Rakin Zeinu <rszeinu@gmail.com>
2026-08-31 13:28:32 +0200

    Rakin verandering 2

--- challenge_day3/Rongze.py
+++ challenge_day3/Rongze.py
@@ -6,15 +6,15 @@ def get_character():
     return "Hans"
 
 def act1(characters):
-    f"At the edge of the bustling night market, {characters[3]} spotted {characters[0]} haggling over an eerie, glowing lantern, while {characters[1]} and {characters[2]} attempted to slip away with the stolen artifact."
+    return f"At the edge of the bustling night market, {characters[3]} spotted {characters[0]} haggling over an eerie, glowing lantern, while {characters[1]} and {characters[2]} attempted to slip away with the stolen artifact."
 def act2(characters):
     return (
         f"{characters[0]} noticed the lantern was glowing brighter, while "
-        f"{characters[3]} chased after {characters[1]} and {characters[2]}, who was runing toward the river."
+        f"{characters[3]} chased after {characters[1]} and {characters[2]}, who was runing toward the ___."
     )
 
 def act3(characters):
     return (
         f"In the end, {characters[0]}, {characters[1]}, {characters[2]}, and {characters[3]} "
-        f"gathered around the lantern and discoverd its light was actually a hidden cave."
+        f"gathered around the lantern and discoverd its light was actually a hidden ___."
     )
