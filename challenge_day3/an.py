038706e2f63a8c1cf90b35e5bcf5b800cbdaa3d3
An Nguyen <tvhome444466@gmail.com>
2026-08-31 11:46:54 +0200

    An added 3 paragraphs

--- challenge_day3/an.py
+++ challenge_day3/an.py
@@ -4,8 +4,24 @@ def get_name():
 def get_team_intro():
     return "This is Team Clock. We are:"
 
+def get_character_intro():
+    return "These are the characters:"
+
 def get_character():
     return 'Ben Dover'
 
 def act1(characters):
     return f"{characters[0]} and {characters[1]} walkd into the forest while {characters[2]} and {characters[3]} waited near the ???."
+
+def act2(characters):
+    return (
+        f"{characters[2]} notice a strange light behind the trees, while "
+        f"{characters[0]}, {characters[1]}, and {characters[3]} tried to find the ???."
+    )
+
+
+def act3(characters):
+    return (
+        f"In the end, {characters[0]}, {characters[1]}, {characters[2]}, and "
+        f"{characters[3]} discoverd that the mysterious light was actually ???."
+    )
--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -5,7 +5,6 @@ import Rongze
 
 
 def introduce_team():
-    print("This is Team Clock. We are:")
     print(an.get_name())
     print(Rakin.get_name())
     print(yutong.get_name())
@@ -13,10 +12,26 @@ def introduce_team():
 
 
 def introduce_characters():
+    print(an.get_character_intro())
     print(an.get_character())
     print(Rakin.get_character())
     print(yutong.get_character())
     print(Rongze.get_character())
 
+
+def print_story():
+    characters = [
+        an.get_character(),
+        Rakin.get_character(),
+        yutong.get_character(),
+        Rongze.get_character()
+    ]
+
+    print(an.act1(characters))
+    print(an.act2(characters))
+    print(an.act3(characters))
+
+
 introduce_team()
 introduce_characters()
+print_story()
