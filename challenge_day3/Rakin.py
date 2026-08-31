0225d332f419e14b2ecd0d2087fdfce447c3de38
An Nguyen <tvhome444466@gmail.com>
2026-08-31 11:30:12 +0200

    Fixing Rakin code of return null

--- challenge_day3/Rakin.py
+++ challenge_day3/Rakin.py
@@ -1,10 +1,8 @@
 def get_name():
     return "Rakin"
 
-print(get_name())
-
 def get_character():
-    return ''
+    return 'Conor McGregor'
 
 def act1(characters):
     return f"{characters[0]} and {characters[1]} walkd into the forest while {characters[2]} and {characters[3]} waited near the ???."
