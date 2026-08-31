5f01e2c15bfc665227b6311d49d96357c6e23869
An Nguyen <tvhome444466@gmail.com>
2026-08-31 10:36:13 +0200

    git_execrise_file_added

---
+++ challenge_day3/git_exercise.py
@@ -0,0 +1,13 @@
+import an
+import Rakin
+import yutong
+
+
+def introduce_team():
+    print("This is Team YourTeamNameHere. We are:")
+    print(an.get_name())
+    print(Rakin.get_name())
+    print(yutong.get_name())
+
+
+introduce_team()
