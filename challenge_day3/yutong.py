fecbe467860e3fab77e431742f644d5b3d80c2fe
Rakin Zeinu <rszeinu@gmail.com>
2026-08-31 13:01:15 +0200

    Rakin Yutong part

--- challenge_day3/yutong.py
+++ challenge_day3/yutong.py
@@ -5,11 +5,14 @@ def get_character():
     return 'Michael Jackson'
 
 def act1(characters):
-    return f"{characters[0]} and {characters[1]} stood on the big stage for their first performance"
+    return f"{characters[0]} and {characters[1]} were stood on the stage for their firt perfprmance"
 
 def act2(characters):
-    return f"the huge fan of the two, {characters[3]}, were sittin in the stands, cherrrring for the two, and ???"
+    return (
+        f"The huge fan of the two, {characters[3]}, was sitting in the stands, "
+        f"cheering for the two, and screaming whenever {characters[3]} performed."
+    )
 
 def act3(characters):
-    return f"after the performance ended, {characters[3]} came to them and asked for a signature, he said with his tears down:Amazing!"
+    return f"after the performence ended, {characters[3]} came to them and asked for a signature, he said with his tears down: ???"
 
