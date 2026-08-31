ecf536e538afaf1ae002af2f560115128969610d
Yutong Wang <yutongw825@gmail.com>
2026-08-31 12:22:27 +0200

    yutong's story uploaded

--- challenge_day3/yutong.py
+++ challenge_day3/yutong.py
@@ -5,4 +5,11 @@ def get_character():
     return 'Michael Jackson'
 
 def act1(characters):
-    return f"{characters[0]} and {characters[1]} walkd into the forest while {characters[2]} and {characters[3]} waited near the ???."
+    return f"{characters[0]} and {characters[1]} were stood on the stage for their firt perfprmance"
+
+def act2(characters):
+    return f"the huge fan of the two, {characters[3]}, were sittin in the stands, cherrrring for the two, and ???"
+
+def act3(characters):
+    return f"after the performence ended, {characters[3]} came to them and asked for a signature, he said with his tears down: ???"
+
