a969623f5e98e0129db1b0e2fe83381431bd6369
Yutong Wang <yutongw825@gmail.com>
2026-08-31 13:08:33 +0200

    Rakin act_1 fix

--- challenge_day3/Rakin.py
+++ challenge_day3/Rakin.py
@@ -7,7 +7,7 @@ def get_character():
     return 'Connor McGregor'
 
 def act1(characters):
-    return f"{characters[0]} smashes {characters[1]} into pieces. {characters[2]} was talking ??? to {characters[3]}. "
+    return f"{characters[0]} smashes {characters[1]} into pieces. {characters[2]} was talking quietly to {characters[3]} about their gap in their strength. "
 def act2(characters):
     return f" The theth of {characters[3]}, was blown out of his ????"
 
