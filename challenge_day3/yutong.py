1fee04162967d047504f65a5ad63e2c53a9e50ef
An Nguyen <tvhome444466@gmail.com>
2026-08-31 12:53:49 +0200

    Fix yutong act1 - real this time

--- challenge_day3/yutong.py
+++ challenge_day3/yutong.py
@@ -5,7 +5,7 @@ def get_character():
     return 'Michael Jackson'
 
 def act1(characters):
-    return f"{characters[0]} and {characters[1]} were stood on the stage for their first performance"
+    return f"{characters[0]} and {characters[1]} stood on the big stage for their first performance"
 
 def act2(characters):
     return f"the huge fan of the two, {characters[3]}, were sittin in the stands, cherrrring for the two, and ???"
