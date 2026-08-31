0eb4e7795e080678c6620bc8c42429ffcf14155d
An Nguyen <tvhome444466@gmail.com>
2026-08-31 13:05:01 +0200

    Fix Rakin 2nd paragraph

--- challenge_day3/Rakin.py
+++ challenge_day3/Rakin.py
@@ -9,7 +9,7 @@ def get_character():
 def act1(characters):
     return f"{characters[0]} smashes {characters[1]} into pieces. {characters[2]} was talking ??? to {characters[3]}. "
 def act2(characters):
-    return f" The theth of {characters[3]}, was blown out of his ????"
+    return f" The teeth of {characters[3]}, was blown out of his hands"
 
 def act3(characters):
     return f" {characters[3]} was taken away to go to the hospitol. {characters[3]} came out the hospital and asked for a ?????"
