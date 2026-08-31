c58df45a1b6555fbcb066eb2f7ef9b6e09bf9baf
韩镕泽 <hanrongze@re-byods-146-50-205-27.wireless.uva.nl>
2026-08-31 12:38:01 +0300

    Renew Rongze.py

--- challenge_day3/Rongze.py
+++ challenge_day3/Rongze.py
@@ -1,4 +1,9 @@
 def get_name():
     return "Rongze"
 
-print(get_name())
+
+def get_character():
+    return "Hans"
+
+def act1(characters):
+    return f"At the edge of the night market, {characters[3]} spotted {characters[0]} hagling over a strange lantern while {characters[1]} and {characters[2]} tryed to slip away with the ___."
