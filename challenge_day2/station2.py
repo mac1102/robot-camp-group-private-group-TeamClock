7ba7c299a4ab88a516bfcae013c29f3b85360faa
Gab1999 <gabriel.irshaid@gmail.com>
2026-09-01 12:19:31 +0400

    ff

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -2,5 +2,4 @@ from datetime import datetime
 
 def solution_station_2(date):
     weekdays = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
-    
-    return weekdays[datetime.strptime(date, "%Y-%m-%d").weekday()]  
+    return weekdays[datetime.strptime(date, "%Y-%m-%d").weekday()]      
