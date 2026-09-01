82b85bec06531242057c2f49f1ce12b707c545ec
Rakin Zeinu <rszeinu@gmail.com>
2026-09-01 14:44:30 +0200

    Check

---
+++ challenge_day2/check.py
@@ -0,0 +1,14 @@
+from station1 import solution_station_1
+from station2 import solution_station_2
+from station3 import solution_station_3
+from station4 import solution_station_4
+from station5 import solution_station_5
+from station6 import solution_station_6
+from station7 import solution_station_7
+
+print(solution_station_1(7), type(solution_station_1(7)))
+print(solution_station_2("2024-03-15"))
+print(solution_station_3(9), solution_station_4(13))
+print(solution_station_5("Nataly"), type(solution_station_5("Nataly")))
+print(solution_station_6(2), type(solution_station_6(2)))
+print(solution_station_7("b*c + d"), type(solution_station_7("b*c + d")))
