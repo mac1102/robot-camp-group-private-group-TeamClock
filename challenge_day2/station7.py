8378560d066d40c0c6042637deb7c9689b19b4c2
Gab1999 <gabriel.irshaid@gmail.com>
2026-09-01 16:18:38 +0400

    station 7 updated

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -6,10 +6,10 @@ def solution_station_7(expression):
         "d": 7,
         "e": 0.5
     }
-
     result = eval(expression, {"__builtins__": {}}, variables)
     return float(result)
 
-print(type(solution_station_7("b*c + d")))  
-print(type(solution_station_7("d * e")))    
-print(type(solution_station_7("d / c")))    
+
+print((solution_station_7("b*c + d")))  
+print((solution_station_7("d * e")))    
+print((solution_station_7("d / c")))    
